a19663e3b3d36b83efe1b476e50c7b9fbaf16b30
Mark Du Preez <dprmar021@myuct.ac.za>
2025-04-24 05:16:17 +0200

    Added functionality for computing, recording and returning waiting times, response times and turnaround times for patrons.

--- src/barScheduling/Patron.java
+++ src/barScheduling/Patron.java
@@ -15,8 +15,17 @@ public class Patron extends Thread {
 
 	private int ID; //thread ID 
 	private int numberOfDrinks;
-	private long waitingTimes;
 
+	// Cumulative waiting time for process/patron.
+	private long averageWaitingTime;
+
+	// Response time for process/patron.
+	private long responseTime;
+
+	// Array of longs representing waiting times for a particular drink/job.
+	private Long[] waitingTimes;
+
+	private long turnaroundTime;
 
 	private DrinkOrder [] drinksOrder;
 	
@@ -26,7 +35,10 @@ public class Patron extends Thread {
 		this.theBarman=aBarman;
 		this.numberOfDrinks=5; // number of drinks is fixed
 		// Array of waiting times for each CPU burst
-		this.waitingTimes = 0;
+		this.averageWaitingTime = 0;
+		this.waitingTimes = new Long [numberOfDrinks];
+		this.responseTime = 0;
+		this.turnaroundTime = 0;
 		drinksOrder=new DrinkOrder[numberOfDrinks];
 		if (seed>0) random = new Random(seed);// for consistent Patron behaviour
 		else random = new Random();
@@ -44,28 +56,56 @@ public class Patron extends Thread {
 	        sleep(arrivalTime);// Patrons arrive at staggered  times depending on ID 
 			System.out.println("+new thirsty Patron "+ this.ID +" arrived"); //Patron has actually arrived
 			//End do not change
+
+			long startTurnaroundTime = System.currentTimeMillis();
 			
 	        for(int i=0;i<numberOfDrinks;i++) {
 	        	drinksOrder[i]=new DrinkOrder(this.ID); //order a drink (=CPU burst)	        
 	        	//drinksOrder[i]=new DrinkOrder(this.ID,i); //fixed drink order (=CPU burst), useful for testing
 				System.out.println("Order placed by " + drinksOrder[i].toString()); //output in standard format  - do not change this
 				theBarman.placeDrinkOrder(drinksOrder[i]);
-				long startWaitingTime = System.nanoTime();
+				long startWaitingTime = System.currentTimeMillis();
 				drinksOrder[i].waitForOrder();
-				long endWaitingTime = System.nanoTime();
-				waitingTimes += endWaitingTime - startWaitingTime;
+				long endWaitingTime = System.currentTimeMillis();
+				waitingTimes[i] += endWaitingTime - startWaitingTime;
+				averageWaitingTime += (endWaitingTime - startWaitingTime)/numberOfDrinks;
+				if (i == 0) {
+					responseTime = endWaitingTime - startWaitingTime;
+				}
 				System.out.println("Drinking patron " + drinksOrder[i].toString());
 				sleep(drinksOrder[i].getImbibingTime()); //drinking drink = "IO"
 			}
 
+			long endTurnaroundTime = System.currentTimeMillis();
+
+			turnaroundTime = endTurnaroundTime - startTurnaroundTime;
+
 			System.out.println("Patron "+ this.ID + " completed ");
+
+
 			
 		} catch (InterruptedException e1) {  //do nothing
 		}
 }
-	public long getWaitingTime () {
-		return waitingTimes;
-	}
-}
-	
 
+// Return reference to array of waiting times for particular patron.
+public Long[] getWaitingTimes () {
+	return waitingTimes;
+}
+
+// Return average waiting time for a particular patron.
+public long getAverageWaitingTime() {
+	return averageWaitingTime;
+}
+
+// Return the response time for a particular patron.
+public long getResponseTime () {
+	return responseTime;
+}
+
+// Return the turnaround time for a particular patron.
+public long getTurnaroundTime() {
+	return turnaroundTime;
+}
+
+}
